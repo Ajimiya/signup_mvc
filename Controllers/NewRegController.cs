674c1b72cadaba83e37d3f8c84f8e5f1a21368f3
unknown <ajimiyaayoobkhan@gmail.com>
2023-06-30 22:44:58 +0530

    Claysys

---
+++ Controllers/NewRegController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MVC_reg_Application.Models;
+using System.Configuration;
+using System.IO;
+
+namespace MVC_reg_Application.Controllers
+{
+    public class NewRegController : Controller
+    {
+        // GET: NewReg
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserClass uc,HttpPostedFileBase file)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(conn);
+            string sqlquery = "insert into [dbo].[MVCregUser](Fname,Lname,Dob,Gender,Phonenumber,Email,Address,State,City,Username,Password,Cpassword,Uimg) values (@Fname,@Lname,@Dob,@Gender,@Phonenumber,@Email,@Address,@State,@City,@Usemame,@Password,@Cpassword,@Uimg)";
+            SqlCommand cmd = new SqlCommand(sqlquery,sqlconn);
+            
+            sqlconn.Open();
+            cmd.Parameters.AddWithValue("@Fname",uc.Fname);
+            cmd.Parameters.AddWithValue("@Lname", uc.Lname);
+            cmd.Parameters.AddWithValue("@Dob", uc.Dob);
+            cmd.Parameters.AddWithValue("@Gender", uc.Gender);
+            cmd.Parameters.AddWithValue("@Phonenumber", uc.Phonenumber);
+            cmd.Parameters.AddWithValue("@Email", uc.Email);
+            cmd.Parameters.AddWithValue("@Address", uc.Address);
+            cmd.Parameters.AddWithValue("@State", uc.State);
+            cmd.Parameters.AddWithValue("@City", uc.City);
+            cmd.Parameters.AddWithValue("@Username", uc.Username);
+            cmd.Parameters.AddWithValue("@Password", uc.Password);
+            cmd.Parameters.AddWithValue("@Cpassword", uc.Cpassword);
+            if(file!=null && file.ContentLength>0)
+            {
+                string filename = Path.GetFileName(file.FileName);
+                string imgpath = Path.Combine(Server.MapPath("~/User-Image/"), filename);
+                file.SaveAs(imgpath);
+            }
+            cmd.Parameters.AddWithValue("@Uimg", "~/User-Image/"+file.FileName);
+            cmd.ExecuteNonQuery();
+            sqlconn.Close();
+            ViewData["Message"] = "User Record" + uc.Fname + "is saved successfully";
+            return View();
+        }
+    }
+}
---
+++ Models/UserClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.OData.Edm;
+
+namespace MVC_reg_Application.Models
+{
+    public class UserClass
+    {
+        [Required (ErrorMessage ="Enter username !")]
+        [Display (Name ="Enter Firstname  : ")]
+        [StringLength(maximumLength:7,MinimumLength =3,ErrorMessage ="Username Length Must be max 7 & Min 3")]
+        public string Fname { get; set; }
+
+        [Required(ErrorMessage ="Please Enter the mail address ! ")]
+        [Display (Name ="Enter Lastname : ")]
+        public string Lname { get; set; }
+        public Date Dob { get; set; }
+        [Required(ErrorMessage ="Please Enter the gender! ")]
+        [Display(Name = "Enter gender  : ")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Please Enter the phone number! ")]
+        [Display(Name = "Enter Phone number   : ")]
+        public string Phonenumber { get; set; }
+        [Required(ErrorMessage ="Please Enter the mail! ")]
+        [Display(Name = "Enter Email Id  : ")]
+        public string  Email { get; set; }
+        [Required(ErrorMessage = "Please Enter the Address! ")]
+        [Display(Name = "Enter the address : ")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please select state! ")]
+        [Display(Name = "Select state ")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "Please select city! ")]
+        [Display(Name = "Select city : ")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please Enter username! ")]
+        [Display(Name = "Enter username  : ")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please Enter password! ")]
+        [Display(Name = "Enter Password  : ")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Please Enter Repassword! ")]
+        [Display(Name = "Enter Repassword  : ")]
+        public string Cpassword { get; set; }
+        [Required(ErrorMessage = "upload profile image! ")]
+        [Display(Name = "profile image  : ")]
+        public string Uimg { get; set; }
+    }
+}
